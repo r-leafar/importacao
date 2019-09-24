7488a7ab066c0649e0bb49ed6d75016c12e444ae
r-leafar <r-leafar@hotmail.com>
2019-09-24 14:10:40 -0300

    annotation para definir o nome da tabela no model

---
+++ src/importacao/annotations/Table.java
@@ -0,0 +1,10 @@
+package importacao.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Table  {
+	
+	public String name();
+}
